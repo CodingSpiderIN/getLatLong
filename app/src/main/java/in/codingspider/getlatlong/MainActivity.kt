b460bb4553b0128c2eafca1a295b75462fe32cae
phpspiderr <phpspiderr@gmail.com>
2022-10-25 14:29:28 +0530

    updated

--- app/src/main/java/in/codingspider/getlatlong/MainActivity.kt
+++ app/src/main/java/in/codingspider/getlatlong/MainActivity.kt
@@ -2,14 +2,15 @@ package `in`.codingspider.getlatlong
 
 import android.Manifest
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
 import android.os.Bundle
+import android.provider.Settings
 import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
@@ -24,9 +25,18 @@ class MainActivity : AppCompatActivity(), LocationListener {
         setContentView(R.layout.activity_main)
         title = "Location App"
         button = findViewById(R.id.fetchLocation)
+        var gpsStatus: Boolean = false
+        val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        gpsStatus = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
         button.setOnClickListener {
-            button.text = "Processing.."
-            getLocation()
+            if (gpsStatus) {
+                button.text = "Processing.."
+                getLocation()
+            } else {
+                val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                startActivity(intent)
+                gpsStatus = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+            }
         }
     }
 
@@ -51,21 +61,20 @@ class MainActivity : AppCompatActivity(), LocationListener {
         tvGpsLocation.text =
             "Latitude: " + location.latitude + " , Longitude: " + location.longitude
         button.text = "Fetch Location"
-        Toast.makeText(this, "Fetch Location", Toast.LENGTH_SHORT).show()
     }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == locationPermissionCode) {
-            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show()
-            } else {
-                Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show()
-            }
-        }
-    }
+/*
+override fun onRequestPermissionsResult(
+requestCode: Int,
+permissions: Array<out String>,
+grantResults: IntArray
+) {
+super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+if (requestCode == locationPermissionCode) {
+if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+tvGpsLocation.text ="Permission Granted"
+} else {
+tvGpsLocation.text ="Permission Denied"
+}
+}
+}*/
 }
